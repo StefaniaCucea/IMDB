92b48210f57020f3128e595939ab03f9ba0313f5
StefaniaCucea <estefania.cucea@gmail.com>
2021-04-29 10:38:28 +0200

    subida de movie.ts

---
+++ movie.ts
@@ -0,0 +1,54 @@
+class Movie{
+
+    public title:string
+    public releaseYear:number
+    public actors:Proffesional[]
+    public nationality: string
+    public director: Proffesional
+    public writer: Proffesional
+    public language: String
+    public platform: string
+    public isMCU: boolean
+    public mainCharacterName: String
+    public producer: Proffesional
+    public distributor: Proffesional
+    public genre: string
+    
+    constructor(titleP:string, releaseYearP:number, nationalityP:string, genreP:string){
+
+        this.title= titleP
+        this.releaseYear=releaseYearP
+        this.actors
+        this.nationality=nationalityP
+        this.director
+        this.writer
+        this.language
+        this.platform
+        this.isMCU
+        this.mainCharacterName
+        this.producer
+        this.distributor
+        this.genre=genreP
+    }
+
+    toString():string
+    {
+        return `The movie's characteristics are:
+                Title: ${this.title}
+                Release year:${this.releaseYear}
+                Actors:${this.actors}
+                Nationality:${this.nationality}
+                Director:${this.director}
+                Writer:${this.writer}
+                Language:${this.language}
+                Plataform:${this.platform}
+                ¿Is MCU?:${this.isMCU}
+                Main's character name :${this.mainCharacterName}
+                Productor:${this.producer}
+                Distribuitor:${this.distributor}
+                Genre:${this.genre}`
+    }
+
+   
+
+}
